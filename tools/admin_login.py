6c8ff8c431bf4219f359640758be051721acdb75
wangjiejia <1481287087@qq.com>
2021-11-08 18:27:07 +0800

    获取yml文件中的数据

--- tools/admin_login.py
+++ tools/admin_login.py
@@ -11,22 +11,31 @@ import pytest
 import requests
 import tools.all_url
 import yaml
-def test_adminLogin():
-    dict1={
-        'username':'admin',
-        'password':'123123'
-    }
-    url = tools.all_url.all_user()
-    res = requests.post(url=url,json=dict1)
-    r=res.json()
-    test_headers=(r['data'])['token']
-    return test_headers
+# def test_adminLogin():
+#     dict1={
+#         'username':'admin',
+#         'password':'123123'
+#     }
+#     url = tools.all_url.all_user()
+#     res = requests.post(url=url,json=dict1)
+#     r=res.json()
+#     test_headers=(r['data'])['token']
+#     return test_headers
+#
+# def test_headers():
+#     token = test_adminLogin()
+#     headers = {'user-agent': 'Mozilla/5.0','content-type': 'application/json;charset=UTF-8','Authorization': 'Bearer {0}'.format(token)}
+#     return headers
+# test_headers()
+from tools.get_yaml import read_yml
 
-def test_headers():
-    token = test_adminLogin()
-    headers = {'user-agent': 'Mozilla/5.0','content-type': 'application/json;charset=UTF-8','Authorization': 'Bearer {0}'.format(token)}
-    return headers
-test_headers()
+def test_adminLogin():
+    url = read_yml()
+    print(url)
+
+
+test_adminLogin()
+
 
 
 
