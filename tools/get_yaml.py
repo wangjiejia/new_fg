b4b7f359450513c3cfc4eee6aded276b8ee6e8fb
wangjiejia <1481287087@qq.com>
2021-11-08 18:06:44 +0800

    获取yml文件中的数据

--- tools/get_yaml.py
+++ tools/get_yaml.py
@@ -7,19 +7,22 @@
 @Motto：ABC(Always Be Coding)
 """
 #获取到yml文件中的数据#
-from tools.file_name import file_name
+from distutils.log import info
 
 import yaml
 import os
 def read_yml():
     with open('all.yml',encoding='UTF-8')as f:
         data = yaml.load(f,Loader=yaml.FullLoader)
-        name="get_yaml"
+        name="admin_loginurl"
+        print(data)
         print(name)
-        name1 = name = os.path.split(__file__)[-1].split(".")[0]
+        print(data.get(name))
+
+        name1 =   os.path.split(__file__)[-1].split(".")[0]
         print(name1)
         if name == name1:
-            print(data)
+            print(data.get(name1))
         else:
             print("在yml文件中未找到对应的条件")
 
