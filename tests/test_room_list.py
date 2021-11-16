09ef6b93b1d4731ed8bde42f150744f1d5e2040c
wangjiejia <1481287087@qq.com>
2021-11-16 16:12:02 +0800

    我的抖音号-抖音号详情-直播记录列表

--- tests/test_room_list.py
+++ tests/test_room_list.py
@@ -7,6 +7,7 @@
 @Motto：ABC(Always Be Coding)
 """
 #我的抖音号-直播记录列表#
+import json
 import os
 import requests
 import pytest
@@ -16,19 +17,24 @@ headers = gettoken()
 url1 = (read_yml()).get(os.path.split(__file__)[-1].split(".")[0])
 print(url1)
 def test_room_list():
-    dict = {
-        "uid": "4340475669264183",
-        "start_at": "2021 - 10 - 01",
-        "end_at": "2021 - 10 - 31",
-        "limit": 20,
-        "page": 1,
-        "export": 1
-    }
-    res = requests.get(url=url1,json=dict,headers=headers)
+
+    url=url1+"?uid=4340475669264183&start_at=2021-10-01&end_at=2021-10-31&limit=20&page=1&export=1"
+    print(url)
+    # dict = {
+    #     "uid": "4340475669264183",
+    #     "start_at": "2021 - 10 - 01",
+    #     "end_at": "2021 - 10 - 31",
+    #     "limit": 20,
+    #     "page": 1,
+    #     "export": 1
+    # }
+    res = requests.get(url=url,headers=headers)
     r = res.json()
-    print(r)
+    assert r['code'] == 200
+    assert (((r['data'])[0])['douyin'])['nickname'] == "花印旗舰店"
 
-test_room_list()
 
+if __name__=='__main__':
+    pytest.main(['test_room_list.py'])
 
 
