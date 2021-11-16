f992b3c031bb7d08a9931c2c6e324d267848f48a
wangjiejia <1481287087@qq.com>
2021-11-16 14:13:17 +0800

    我的直播间-侧边栏我的抖音号

--- tests/test_douyin.py
+++ tests/test_douyin.py
@@ -8,7 +8,6 @@
 """
 #抖音号详情#
 #使用生成的用户对应的抖音号，查看抖音号详情#
-
 from tools.get_yaml import read_yml
 import os
 import pytest
@@ -18,15 +17,31 @@ from tools.sql import select_sql1
 uid = select_sql1("SELECT uid from douyin where manager_id = (select id from manager where username='wj_test' order by id desc) and off_at is null")
 headers = gettoken()
 url1 = (read_yml()).get(os.path.split(__file__)[-1].split(".")[0])
-def douyin():
-    dict={
-        "uid":uid[0]
-    }
-    print(dict)
-    res = requests.post(url=url1,json=dict,headers=headers)
+def test_douyin():
+    url = url1+"/"+uid[0]
+
+    res = requests.get(url=url,headers=headers)
     r = res.json()
+    assert r['code'] == 200
+    assert (((r['data'])['list'])[0])['nickname'] == '花印旗舰店'
+
     print(r)
 
-douyin()
+
+#我的直播间-侧边栏抖音号展示#
+def test_douyin_1():
+    url = url1 +  "limit=1000"+"&"+"live_end=1"
+
+    res = requests.get(url=url,headers=headers)
+    r=res.json()
+    assert r['code'] == 200
+    assert (((r['data'])['list'])[0])['nickname'] == "花印旗舰店"
+
+
+# test_douyin()
+
+
+if __name__=='__main__':
+    pytest.main(['test_douyin.py'])
 
 
