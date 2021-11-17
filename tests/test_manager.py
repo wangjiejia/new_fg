f6d8553ecb63c82e23c46ae0691dd4ba95fd3249
wangjiejia <1481287087@qq.com>
2021-11-17 16:27:28 +0800

    团队管理-查看管理员信息

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -9,6 +9,7 @@
 #查看管理员账户信息#
 from tools.gettoken import gettoken
 from tools.get_yaml import read_yml
+from tools.sql import select_sql
 import os
 import requests
 import json
@@ -23,12 +24,17 @@ def test_manager():
     print(json.dumps(r,indent=4))
     assert r['code'] == 200
     assert  (r['data'])['username'] == 'wj_test'
+    assert (r['data'])['name'] == '管理员'
     # end_time = datetime.date.today() + datetime.timedelta(days=30)
     #因为新增了一笔1个月的订单，所以过期时间等于当前时间往后+1个月#
     # expried_time = datetime((r['data'])['expired_at'])
     #
     # time1 = int(datetime.time.strftime("%Y%M%D", expried_time))
     # time2 = int(datetime.time.strftime("%Y%M%D", time3))
+
+def test_update_manager_name():
+    manager_id = select_sql("select id from manager where username='wj_test' and  deleted_at is null order by id desc ")
+
 
 
 
