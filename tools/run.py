8e72fdfbf7c64938dbf46a74ad0afb2e32a9f543
wangjiejia <1481287087@qq.com>
2021-11-17 10:25:14 +0800

    添加allure---执行生成报告

--- tools/run.py
+++ tools/run.py
@@ -11,18 +11,24 @@ import pytest
 import report
 import rerun as rerun
 import allure
+import os
 def run():
     print("执行开始")
 if __name__=='__main__':
-    pytest.main(["-v", "-s",
-                 "./test_admin_login.py","./test_create_user.py",
-                 "./test_update_version.py","./gettoken.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_user_login.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_add_douyin.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_add_room.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin_list.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin_room.py",
-                 # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin_expired.py",
-                 # "./test_delete_user.py"
-                 '--alluredir', './report/xml'])
+    # pytest.main(["-v", "-s",
+    #              "./test_admin_login.py","./test_create_user.py",
+    #              "./test_update_version.py","./gettoken.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_user_login.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_add_douyin.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_add_room.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin_list.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin_room.py",
+    #              # "C:\/Users\/1\/PycharmProjects\/new_fg\/tests\/test_douyin_expired.py",
+    #              # "./test_delete_user.py"
+    #              '--alluredir', './report/xml'])
+
+    pytest.main(["-v", "-s",'--alluredir', './Outputs/result','./test_admin_login.py'])
+    split = 'allure'+'generate'+'./Outputs/result'+'o'+'./Outputs/report' + '--clean'
+    os.system(split)
+
