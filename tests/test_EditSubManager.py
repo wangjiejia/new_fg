0effc83ca6d67ed9ec823248934e26a1ea0d444a
wangjiejia <1481287087@qq.com>
2021-11-22 18:03:54 +0800

    员工列表搜索

--- tests/test_EditSubManager.py
+++ tests/test_EditSubManager.py
@@ -14,8 +14,20 @@ from tests.test_sub_manager import test_sub_manager
 manager_id = test_sub_manager()
 headers=gettoken()
 url = (read_yml()).get(os.path.split(__file__)[-1].split(".")[0])
+print(url)
+id = test_sub_manager()
+print(id)
 def test_EditSubManager():
+    # url = url +"/"
     dict={
-        "id":
+        "id":id
     }
-    res = requests.post(url=)
+    print(dict)
+    res = requests.get(url=url,json=dict,headers=headers)
+    r=res.json()
+    print(r)
+    assert r['code'] == 200
+    print((r['data'])['id'])
+    # assert (r['data'])['id'] == id
+
+test_EditSubManager()
--- tests/test_sub_manager.py
+++ tests/test_sub_manager.py
@@ -10,25 +10,28 @@
 
 import os
 import requests
+import json
 from tools.gettoken import gettoken
 from tools.get_yaml import read_yml
 url = (read_yml()).get(os.path.split(__file__)[-1].split(".")[0])
 headers=gettoken()
 def test_sub_manager():
     url = "https://live-tools-qa1.youfenba.com/api/v1/sub_manager?page=1&name=&status=0"
-    dict={
-        "page":1,
-        "name":" ",
-        "status":1
-    }
+    # dict={
+    #     "page":1,
+    #     "name":" ",
+    #     "status":1
+    # }
     # url = url+"?"+"page"+"="+dict['page']+"&"+"name"+"="+"&"+"status"+"="+str(0)
     # print(url)
 
     res = requests.get(url=url,headers=headers)
     r=res.json()
     assert r['code'] == 200
+    a = ((((r['data'])['list']))[0])['id']
+    print(a)
+    return a
 
-    print(((r['data'])['list'])[0].)
 
     #查找冻结的员工#
 def test_sub_manager_1():
